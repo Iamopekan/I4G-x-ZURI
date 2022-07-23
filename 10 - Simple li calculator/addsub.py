1e8d630f2b99da0aa0480affe085c4b92dfd0f5e
Ope Ajayi <105161691+Iamopekan@users.noreply.github.com>
2022-07-23 20:16:39 +0100

    Add files via upload

---
+++ 10 - Simple li calculator/addsub.py
@@ -0,0 +1,13 @@
+a=float(input("Enter first number here "))
+b=float(input("Enter second number here "))
+
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+
+print("Addition is =", add)
+print("Subtraction is =", sub)
+print("Multiplication is =", mul)
+print("Division is =", div)
+
---
+++ 10 - Simple li calculator/calculator.py
@@ -0,0 +1,10 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====== RESTART: C:/Users/Designhive/pyproj/Simple li calculator/addsub.py ======
+Enter first number here 100
+Enter second number here 250
+Addition is = 350.0
+Subtraction is = -150.0
+Multiplication is = 25000.0
+Division is = 0.4
